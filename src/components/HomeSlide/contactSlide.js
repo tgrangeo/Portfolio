0f756511ecc3a30735eef5771cf5f0f47297f120
tgrangeo <thomas.grangeon9@gmail.com>
2023-07-04 12:36:43 +0200

    fix contact

--- src/components/HomeSlide/contactSlide.js
+++ src/components/HomeSlide/contactSlide.js
@@ -3,45 +3,16 @@ import { Send } from '@mui/icons-material'
 import "../../styles/HomeSlide/HomeSlide.css"
 
 export default function ContactSlide() {
-
-	const [name, setName] = useState('');
-	const [mail, setMail] = useState('');
+	const [object, setObject] = useState('');
 	const [message, setMessage] = useState('');
-
-	function submit() {
-		console.log(`${name} ${mail} ${message}`);
-		if (name && mail && message) {
-			const requestBody = {
-				name: name,
-				mail: mail,
-				message: message
-			}
-			const requestOptions = {
-				method: 'POST',
-				mode: 'cors',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(requestBody)
-			};
-			fetch('http://localhost:8080/api/v1/contact', requestOptions)
-		}
-		//.then(response => response.json())
-		// .then(data => this.setState({ postId: data.id }));
-	}
 
 	return (
 		<div className='contact'>
 			<div className="slideTitle">Contact Me</div>
-			{/* <form className="formDiv" onSubmit={submit}> */}
 			<div className="form-group">
-				<input type="text" placeholder='Name' className="form-control" value={name}
+				<input type="text" className="form-control" placeholder='Object' value={object}
 					onChange={(event) =>
-						setName(event.target.value)
-					} />
-			</div>
-			<div className="form-group">
-				<input type="email" className="form-control" placeholder='Email' aria-describedby="emailHelp" value={mail}
-					onChange={(event) =>
-						setMail(event.target.value)
+						setObject(event.target.value)
 					} />
 			</div>
 			<div className="form-group">
@@ -51,9 +22,10 @@ export default function ContactSlide() {
 					}></textarea>
 			</div>
 			<div className="form-group">
-				<button type="submit" onClick={submit} className="SendButton"><Send style={{ marginRight: "10px" }} />Send</button>
+				<a href={`mailto:thomas.grangeon9+Portfolio@gmail.com?subject=${object} &body=${message}`}>
+					<div className="SendButton"><Send style={{ marginRight: "10px" }} /> Send</div>
+				</a>
 			</div>
-			{/* </form> */}
 		</div>
 	)
 }
